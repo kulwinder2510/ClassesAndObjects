99ee1139f80023e98d2353d10cc803b8c1a739bd
kulwinder2510 <125046449+kulwinder2510@users.noreply.github.com>
2023-02-23 16:04:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-this is my first repository.
-thank you.
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My First JavaScript</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
+</body>
+</html> 
